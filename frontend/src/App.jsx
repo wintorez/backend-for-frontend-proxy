65b247bb0c689dbfcc0d55e173518b5626def7c2
Reza Sadr <wintorez@gmail.com>
2025-05-21 15:50:27 -0400

    Update status messages in App component for clarity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,14 @@ function App() {
     <div>
       <h1>Backend for Frontend Proxy</h1>
       <Fetcher title="Calling backend directly" url="http://localhost:3002/" />
-      <p>* Failed due to missing CORS</p>
+      <p>* Will fail due to missing CORS</p>
       <Fetcher title="Calling bff directly" url="http://localhost:3001/" />
-      <p>* Success due to added CORS</p>
+      <p>* Will pass due to added CORS</p>
       <Fetcher
         title="Calling backend via proxy"
         url="http://localhost:3001/proxy/"
       />
-      <p>* Success via proxy</p>
+      <p>* Will pass via proxy</p>
     </div>
   )
 }
